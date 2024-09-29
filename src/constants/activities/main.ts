f725e2b7425125a428ddfc9e95a336e3c0bd5556
simon0820s <simon0820s@gmail.com>
2024-09-29 14:40:07 -0500

    feat(activities): create main window

--- src/constants/activities/main.ts
+++ src/constants/activities/main.ts
@@ -1,3 +1,6 @@
+import vendor1Image from '@/public/images/vendors/Frame 1000001685.svg'
+import vendor2Image from '@/public/images/vendors/image 14.svg'
+
 export const activitiesActionCards = [
   {
     icon: 'add_circle',
@@ -15,5 +18,18 @@ export const activitiesActionCards = [
     textButton: 'Update',
     isSecondary: true,
     href: '/my-organization/create',
+  },
+]
+
+export const vendors = [
+  {
+    imgSrc: vendor1Image,
+    title: 'BRGR HAUS',
+    description: 'Kura Hulanda Village, 3 Langestraat',
+  },
+  {
+    imgSrc: vendor2Image,
+    title: 'Esperamos Caracasbaai',
+    description: 'Caracasbaaiweg 280 Unit 10, Eden Mall',
   },
 ]
