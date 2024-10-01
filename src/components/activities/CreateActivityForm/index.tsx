e690383d86604f841299086c0c4a88c6dc89a49a
simon0820s <simon0820s@gmail.com>
2024-10-01 03:07:42 -0500

    feat(activityForm): map all form

--- src/components/activities/CreateActivityForm/index.tsx
+++ src/components/activities/CreateActivityForm/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardContent, Divider, Link, Stack, Typography } from '@mui/material'
+import { Button, Card, CardContent, Divider, Icon, InputAdornment, Link, Stack, Typography } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import React from 'react'
 
@@ -7,17 +7,47 @@ export default function CreateActivityForm() {
     <Card>
       <CardContent sx={{ padding: '0px' }}>
         <Stack padding="24px">
-          <Typography variant="h3">What’s the name of your activity?</Typography>
-          <TextField id="activityName" variant="outlined" label="Beach Cleanup" />
+          <TextField
+            id="activityName"
+            variant="outlined"
+            label="What’s the name of your activity?"
+            placeholder="Beach Cleanup"
+          />
         </Stack>
-        <Stack padding="24px">
+        <Stack padding="24px" gap="16px">
           <Typography variant="h3">When does your activity start and end?</Typography>
+          <Stack direction="row" gap="16px">
+            <TextField id="date" type="date" variant="outlined" placeholder="Date" />
+            <TextField id="startTime" type="time" variant="outlined" placeholder="Start time" />
+            <TextField id="endTime" type="time" variant="outlined" placeholder="End time" />
+          </Stack>
         </Stack>
         <Stack padding="24px">
-          <Typography variant="h3">Where is it located?</Typography>
+          <TextField
+            id="location"
+            type="search"
+            variant="outlined"
+            label="Where is it located?"
+            placeholder="Enter location"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon>search</Icon>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
         </Stack>
         <Stack padding="24px">
-          <Typography variant="h3">What can the attendee expect?</Typography>
+          <TextField
+            id="activityName"
+            variant="outlined"
+            label="What can the attendee expect?"
+            placeholder="Describe your activity"
+            multiline
+          />
         </Stack>
         <Divider />
         <Stack direction="row" padding="24px 24px 0px 24px" justifyContent="end">
