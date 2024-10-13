d9eef4b28b9dcce143689bbc9cf6f50bc7e38391
JamesTovarR04 <jamestovar09@hotmail.com>
2024-10-12 23:14:28 -0500

    feat(config): add @svgr/webpack to support SVG loading

--- src/types/svgr.d.ts
+++ src/types/svgr.d.ts
@@ -1,10 +1,10 @@
 declare module '*.svg' {
-  import { FC, SVGProps } from 'react'
+  import { type FC, type SVGProps } from 'react'
   const content: FC<SVGProps<SVGElement>>
   export default content
 }
 
 declare module '*.svg?url' {
-  const content: any
+  const content: any // eslint-disable-line
   export default content
 }
