66e622c0915b33593c51d1005a61c14ac7e408ec
simon0820s <simon0820s@gmail.com>
2024-12-02 12:07:58 -0500

    feat(selectRequestsAttendance): complete mock view

--- src/components/activities/Payout/AttendanceRequests/Card/index.tsx
+++ src/components/activities/Payout/AttendanceRequests/Card/index.tsx
@@ -1,5 +1,5 @@
 import { type AttendanceRequest } from '@/types/activities'
-import { Accordion, AccordionDetails, AccordionSummary, Stack, Typography } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Checkbox, Icon, Stack, Typography } from '@mui/material'
 import Image from 'next/image'
 import DownIcon from '@/public/images/icons/down.svg?url'
 import React from 'react'
@@ -7,7 +7,11 @@ import React from 'react'
 type Props = {
   request: AttendanceRequest
 }
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } }
 export default function AttendanceRequestCard({ request }: Props) {
+  const handleSelect = (event: React.MouseEvent) => {
+    event.stopPropagation()
+  }
   return (
     <Stack>
       <Accordion>
@@ -15,24 +19,48 @@ export default function AttendanceRequestCard({ request }: Props) {
           aria-controls="panel1-content"
           id="panel1-header"
           expandIcon={<Image src={DownIcon} alt="downIcon" />}
+          sx={{ flexDirection: 'row-reverse', gap: '32px' }}
         >
-          <Stack gap="10px">
-            <Typography variant="h3">{request.user}</Typography>
-            <div style={{ display: 'grid', gap: '10px', gridTemplateColumns: 'auto 1fr' }}>
-              <Typography color="#7E7E7E">Check in:</Typography>
-              <Typography color="#353535">{request.checkIn}</Typography>
-              <Typography color="#7E7E7E">Check out:</Typography>
-              <Typography color="#353535">{request.checkOut}</Typography>
-            </div>
+          <Stack direction="row" gap="10px" justifyContent="space-between" alignItems="center" width="100%">
+            <Stack gap="10px">
+              <Typography variant="h3">{request.user}</Typography>
+              <div style={{ display: 'grid', gap: '10px', gridTemplateColumns: 'auto 1fr' }}>
+                <Typography color="#7E7E7E">Check in:</Typography>
+                <Typography color="#353535">{request.checkIn}</Typography>
+                <Typography color="#7E7E7E">Check out:</Typography>
+                <Typography color="#353535">{request.checkOut}</Typography>
+              </div>
+            </Stack>
+            <Checkbox
+              onClick={handleSelect}
+              {...label}
+              icon={<Icon>radio_button_unchecked</Icon>}
+              checkedIcon={<Icon>radio_button_checked</Icon>}
+            />
           </Stack>
         </AccordionSummary>
-        <AccordionDetails>
-          <div style={{ display: 'grid', gap: '10px', gridTemplateColumns: 'auto 1fr' }}>
-            <Typography color="#7E7E7E">PoC:</Typography>
-            <Typography color="#353535">{request.Poc}</Typography>
-            <Typography color="#7E7E7E">PoC Image:</Typography>
-            <Typography color="#353535">{request.PocImage}</Typography>
-          </div>
+        <AccordionDetails sx={{ marginLeft: '32px' }}>
+          <Stack direction="row">
+            <Image style={{ visibility: 'hidden' }} src={DownIcon} alt="downIcon" />
+            <div style={{ display: 'grid', gap: '10px', gridTemplateColumns: 'auto 1fr' }}>
+              <Typography color="#7E7E7E">PoC:</Typography>
+              <Typography color="#353535">{request.Poc}</Typography>
+              <Typography color="#7E7E7E">PoC Image:</Typography>
+              <Box
+                width={135}
+                height={84}
+                sx={{ backgroundColor: '#F2F2F2', borderRadius: '12px' }}
+                justifyContent="center"
+                alignItems="center"
+                display="flex"
+              >
+                <Typography variant="subtitle2" textTransform="uppercase" color="#A9A9A9">
+                  PoC Image
+                </Typography>
+              </Box>
+              <Typography color="#353535">{}</Typography>
+            </div>
+          </Stack>
         </AccordionDetails>
       </Accordion>
     </Stack>
--- src/components/activities/Payout/AttendanceRequests/SelectionCard/index.tsx
+++ src/components/activities/Payout/AttendanceRequests/SelectionCard/index.tsx
@@ -12,7 +12,7 @@ export default function AttendanceRequestsSelectionCard() {
       checkIn: '10:01AM',
       checkOut: '12:01AM',
       Poc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using. Helped Yolanda with the trash bag transportation to the nearest desp',
-      PocImage: 'imagen',
+      PocImage: '',
     },
     {
       user: 'Jhonny Bobs',
@@ -26,7 +26,7 @@ export default function AttendanceRequestsSelectionCard() {
       checkIn: '10:01AM',
       checkOut: '12:01AM',
       Poc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using. Helped Yolanda with the trash bag transportation to the nearest desp',
-      PocImage: 'imagen',
+      PocImage: '',
     },
   ]
   if (!attendanceRequests)
