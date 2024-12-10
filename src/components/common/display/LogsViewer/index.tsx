8a123e6327924020c9a7dff025e902573f311cbb
voyager-seb <seb@kolektivo.co>
2024-12-09 20:28:32 -0500

    feat(impact) flag resource issue

--- src/components/common/display/LogsViewer/index.tsx
+++ src/components/common/display/LogsViewer/index.tsx
@@ -2,7 +2,7 @@ import { type ReactElement } from 'react'
 import { Box, Stack, Typography, Icon } from '@mui/material'
 import { ImpactDto } from '@/types/activities'
 import Image from 'next/image';
-import KTFlag from '@/public/images/ktflag.svg'
+import KTFlag from '/public/images/ktflag.svg'
 
 type ItemLog = ImpactDto & {
   icon?: string
