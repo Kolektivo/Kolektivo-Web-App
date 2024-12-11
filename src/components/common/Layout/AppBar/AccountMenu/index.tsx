db342505034b07a226cf787c0eff7f2780127786
voyager-seb <seb@kolektivo.co>
2024-12-11 00:15:27 -0500

    feat(avatar): improving hover shadow

--- src/components/common/Layout/AppBar/AccountMenu/index.tsx
+++ src/components/common/Layout/AppBar/AccountMenu/index.tsx
@@ -44,7 +44,14 @@ const AccountMenu = (): ReactElement => {
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
       >
-        
+        <Box
+          sx={{
+            position: 'relative', 
+            display: 'inline-block', 
+            width: 5,
+            height: 48,
+          }}
+        ></Box>
         <Avatar
           alt="user-avatar"
           src={user?.user_metadata.avatar_url}
